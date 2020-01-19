38dbd4785df7e796f1722560db0a02a51d3021c5
Sagar Pant <spant786@gmail.com>
2020-01-19 14:00:00 +0100

    Admin Header

---
+++ src/Component/Admin/AdminHeader.js
@@ -0,0 +1,76 @@
+
+import React from'react'
+import {NavLink,Route,Switch,Redirect} from 'react-router-dom'
+import '../../css/AdminHeader.css'
+import AdminDashboard from './AdminDashboard'
+import AddProduct from'./AddProduct'
+import ManageProduct from './ManageProduct'
+import ManageUser from './ManageUser'
+import ManageCart from './ManageCart'
+import UpdateProduct from './UpdateProduct'
+import UpdateUser from './UpdateUser'
+import Logout from './Logout'
+
+
+class Sidenav extends React.Component{
+    state = {
+
+        headersdata22: {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        },
+        user_id:"/AdminDashboard/"+localStorage.getItem('user_id'),
+        user_id1:"/AddProduct/"+localStorage.getItem('user_id'),
+        user_id2:"/ManageCart/"+localStorage.getItem('user_id'),
+        user_id3:"/ManageUser/"+localStorage.getItem('user_id'),
+        user_id4:"/ManageProduct/"+localStorage.getItem('user_id'),
+        user_id5:"/UpdateProduct/"+localStorage.getItem('user_id'),
+        user_id6:"/UpdateUser/"+localStorage.getItem('user_id'),
+       
+
+        
+
+    }
+    render(){
+        if (this.state.headersdata22.headers.Authorization === "Bearer null") {
+            alert('Login! First')
+            return <Redirect to='/Login' />
+        }
+        return(
+            <div class="navbar">
+            <ul>
+                <li>  <NavLink to={ `${this.state.user_id}` }>AdminDashboard</NavLink></li>
+                <li><NavLink to= { `${this.state.user_id1}` } >AddProduct</NavLink></li>
+                <li><NavLink to={ `${this.state.user_id2}` }>ManageCart</NavLink></li>
+                <li><NavLink to={ `${this.state.user_id3}` }>ManageUser</NavLink></li>
+                <li><NavLink to={ `${this.state.user_id4}` }>ManageProduct</NavLink></li>
+            
+                <li><NavLink to={ `${this.state.user_id6}` }>Update User</NavLink></li>
+                <li><NavLink to="/logout" >Logout</NavLink></li>
+               
+               
+            
+
+
+            </ul>
+
+            <Switch>
+                <Route path="/AdminDashBoard" exact component={AdminDashboard} />
+                <Route path="/AddProduct" exact component={AddProduct} />
+                <Route path="/ManageCart" exact component={ManageCart} />
+                <Route path="/ManageUser" exact component={ManageUser} />
+                <Route path="/ManageProduct" exact component={ManageProduct} />
+                <Route path="/UpdateProduct" exact component={UpdateProduct} />
+                <Route path="/UpdateUser" exact component={UpdateUser} />
+            
+                <Route path="/logout" exact component={Logout} />
+             
+
+            </Switch>
+
+
+        </div>
+        )
+    }
+
+}
+export default Sidenav
