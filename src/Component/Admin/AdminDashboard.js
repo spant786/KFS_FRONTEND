52c2d1584646732bb29516466c083aefebdd6f7e
Sagar Pant <spant786@gmail.com>
2020-01-19 14:00:00 +0100

    Admin Dashboard Added

---
+++ src/Component/Admin/AdminDashboard.js
@@ -0,0 +1,41 @@
+
+import React from  'react'
+import axios from 'axios'
+import {NavLink,Switch,Route,Redirect} from 'react-router-dom'
+import '../../css/Adminbody.css'
+import AdminHeader from './AdminHeader'
+
+
+class AdminDashboard extends React.Component{
+    state = {
+
+        headersdata22: {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        }
+      
+
+    }
+    LogOut = () => {
+        //delete token in browser and logout from backend
+        axios.post('http://localhost:5000/logout')
+        localStorage.removeItem('token')
+        window.location.href = '/login'
+
+
+    }
+        
+    render(){
+        if (this.state.headersdata22.headers.Authorization === "Bearer null") {
+            alert('Login! First')
+            return <Redirect to='/Login' />
+        }
+        
+    return(
+        <div>
+        <AdminHeader/>
+        <div><h1>Welcome to AdminDashboard</h1></div>
+        </div>
+    )
+    }
+}
+export default AdminDashboard
