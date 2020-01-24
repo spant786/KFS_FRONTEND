bf02a10201e4d6d61fdf963a92690587c62c0cec
Sagar Pant <spant786@gmail.com>
2020-01-24 14:00:00 +0100

    user header

---
+++ src/Component/User/UserHead.js
@@ -0,0 +1,59 @@
+
+import React from'react'
+import {NavLink,Route,Switch,Redirect} from 'react-router-dom'
+import '../../css/AdminHeader.css'
+import UserDashboard from './UserDashboard'
+import ViewProduct from'./ViewProduct'
+import UpdateProfile from './UpdateProfile'
+import Logout from '../Admin/Logout'
+
+
+class Sidenav extends React.Component{
+    state = {
+
+        headersdata22: {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        },
+        user_id:"/UserDashboard/"+localStorage.getItem('user_id'),
+        user_id1:"/ViewProduct/"+localStorage.getItem('user_id'),
+        user_id2:"/UpdateProfile/"+localStorage.getItem('user_id'),
+       
+
+        
+
+    }
+    render(){
+        if (this.state.headersdata22.headers.Authorization === "Bearer null") {
+            alert('Login! First')
+            return <Redirect to='/Login' />
+        }
+        return(
+            <div class="navbar">
+            <ul>
+                <li>  <NavLink to={ `${this.state.user_id}` }>UserDashboard</NavLink></li>
+                <li><NavLink to= { `${this.state.user_id1}` } >View Product</NavLink></li>
+                <li><NavLink to={ `${this.state.user_id2}` }>Update profile</NavLink></li>
+                <li><NavLink to="/logout" >Logout</NavLink></li>
+               
+               
+            
+
+
+            </ul>
+
+            <Switch>
+                <Route path="/UserDashBoard" exact component={UserDashboard} />
+                <Route path="/ViewProduct" exact component={ViewProduct} />
+                <Route path="/UpdateProfile" exact component={UpdateProfile} />
+                <Route path="/logout" exact component={Logout} />
+             
+
+            </Switch>
+
+
+        </div>
+        )
+    }
+
+}
+export default Sidenav
